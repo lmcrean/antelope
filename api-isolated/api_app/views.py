23b9e09345e4dc016076ffd132c6629437d171aa
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-02-17 15:52:35 +0000

    Add views.py with basic API test endpoint and custom error handlers

---
+++ api-isolated/api_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse
+
+# Create your views here.
+
+class APITest(APIView):
+    def get(self, request):
+        return JsonResponse({"message": "API is working!"})
+
+def custom_error_404(request, exception):
+    return JsonResponse({'error': '404 error, Not Found'}, status=404)
+
+def custom_error_500(request):
+    return JsonResponse({'error': '500 error, Internal Server Error'}, status=500)
